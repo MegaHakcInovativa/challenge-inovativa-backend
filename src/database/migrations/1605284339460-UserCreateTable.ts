6700cf41ede77d4453477b1fc9bdb8a0c6d65d4f
Maycon Queiroz <fmaycon@gmail.com>
2020-11-13 13:33:37 -0300

    feature: created connection db and migration/model user

---
+++ src/database/migrations/1605284339460-UserCreateTable.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class UserCreateTable1605284339460 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'telephone',
+            type: 'varchar',
+          },
+          {
+            name: 'location',
+            type: 'varchar',
+          },
+          {
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'Now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'Now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
